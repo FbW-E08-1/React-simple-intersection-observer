cbc23a299b28d57dd8e19ea8db4786d6e65a1a2d
jsk900 <jsk900@yahoo.co.uk>
2022-03-23 12:21:10 +0100

    E08 React simple-Intersection observer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
 import './App.css';
 
 const App = () => {
+  const secRef = useRef();
+  const anotherRef = useRef();
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1,
+    };
+
+    const handleIntersect = (entries) => {
+      entries.map((entry) => entry.isIntersecting && console.log(entry));
+    };
+
+    const observer = new IntersectionObserver(handleIntersect, options);
+
+    observer.observe(secRef.current);
+    observer.observe(anotherRef.current);
+  }, []);
+
   return (
-    <main>
-     <h1>Welcome to React</h1>
-    </main>
+    <>
+      <main>
+        <h1>Intersection observer</h1>
+      </main>
+
+      <section ref={secRef}>
+        <h1>Section</h1>
+      </section>
+      <section ref={anotherRef}>
+        <h1>Another section</h1>
+      </section>
+    </>
   );
-}
+};
 
 export default App;
-
